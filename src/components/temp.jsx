56529ccd05a66befa0c2cb804c2b2883f94446e0
Pavel M <april504@yandex.ru>
2023-09-29 20:35:58 +0700

    wip: proof of concept

--- src/components/temp.jsx
+++ src/components/temp.jsx
@@ -12,6 +12,7 @@ import {
   useMultiStyleConfig,
   Spinner
 } from '@chakra-ui/react'
+import { useState, useRef } from 'react';
 
 import Lorem from 'react-lorem-component'
 
@@ -21,9 +22,44 @@ const [StylesProvider] = createStylesContext('Menu');
 export default function Temp() {
   const { isOpen, onOpen, onClose } = useDisclosure()
 
+  const videoRef = useRef(null);
+
+  function videoHandler(e) {
+      const video = e.target
+      const targetTime = 2.4;
+      
+      const rounded = Math.round(video.currentTime * 10) / 10;
+
+      if (rounded == targetTime) {
+        e.target.pause();
+        handleOpen();
+      }
+  }
+
+  function closeHandler() {
+    onClose()
+    videoRef.current.play();
+  }
+
+  function handleOpen() {
+    setSrc(undefined)
+    onOpen()
+    fetch('https://picsum.photos/700/467')
+      .then(res => res.blob())
+      .then(blob => {
+        const url = URL.createObjectURL(blob)
+        setSrc(url)
+      })
+  }
+
   const styles = useMultiStyleConfig('Modal', {
     variant: 'no-scroll',
   })
+
+  const [src, setSrc] = useState('')
+
+  // useEffect(() => {
+  // }, [])
 
   return (
     <div className="my-thing">
@@ -32,15 +68,18 @@ export default function Temp() {
       <div className="map">
         <video className="bg-video" autoPlay muted loop>
           <source type='video/mp4' src="koi.mp4" />
+        </video>   
+        <video className="bg-video blend" autoPlay muted loop onTimeUpdate={videoHandler} ref={videoRef}>
+          <source type='video/webm' src="demo.webm" />
         </video>        
       </div>
 
       <div className="btn-wrapper">
-        <button className="link" onClick={onOpen}>open</button>
+        <button className="link" onClick={handleOpen}>open</button>
       </div>
 
       <StylesProvider value={styles.modal}>
-        <Modal isOpen={isOpen} onClose={onClose}>
+        <Modal isOpen={isOpen} onClose={closeHandler}>
           <ModalOverlay />
           <ModalContent sx={{'--chakra-sizes-md': '1024px'}}>
             <ModalHeader>Открой еще раз, и будет новый контент</ModalHeader>
@@ -48,7 +87,7 @@ export default function Temp() {
             <ModalBody>
               <div className="modal-flex">
                 <div className="modal__img-wrap">
-                  <img src="https://picsum.photos/700/467" className="modal-img"/>                  
+                  <img src={src} className="modal-img"/>
                   <Spinner color='red.500' className="modal-spinner"/>
                 </div>
                 <Lorem count={5} seed={Math.floor(Math.random() * 100)}/>
